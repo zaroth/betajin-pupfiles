3012bbbe148e82a8c7065e75a27cd523995dc223
Lukasz Dobrogowski <lukasz.dobrogowski@gmail.com>
2016-10-01 08:20:37 +0000

    cleaned up serving logs, enabled mongodb service

--- modules/jinteki/files/scripts/rebuild.sh
+++ modules/jinteki/files/scripts/rebuild.sh
@@ -12,31 +12,36 @@ LOG_DIR="${ROOT_DIR}/logs"
 LOG_FILE="${LOG_DIR}/$(date +%Y%m%d_%H%M)_build.log"
 START_TIME=`date +%s`
 
+function logmsg {
+  echo "${1}"
+  echo -en "\n\n${1}\n\n" >> $LOG_FILE
+}
+
 cd $REPO_DIR
 
-echo -en "Pulling fresh changes from GitHub...\n\n" | tee -a $LOG_FILE
+logmsg "Pulling fresh changes from GitHub..."
 git pull origin dev &>> $LOG_FILE
 
-echo -en "\n\nUpdating npm packages...\n\n" | tee -a $LOG_FILE
+logmsg "Updating npm packages..."
 npm update &>> $LOG_FILE
 
-echo -en "\n\nUpdating bower packages...\n\n" | tee -a $LOG_FILE
+logmsg "Updating bower packages..."
 bower update &>> $LOG_FILE
 
-echo -en "\n\nPulling new cards from NRDB...\n\n" | tee -a $LOG_FILE
+logmsg "Pulling new cards from NRDB..."
 coffee "data/fetch.coffee" &>> $LOG_FILE
 
-echo -en "\n\nCleaning up previous build...\n\n" | tee -a $LOG_FILE
+logmsg "Cleaning up previous build..."
 lein clean &>> $LOG_FILE
 
-echo -en "\n\nCompiling Stylus files...\n\n" | tee -a $LOG_FILE
+logmsg "Compiling Stylus files..."
 stylus src/css/ -o resources/public/css/ &>> $LOG_FILE
 
-echo -en "\n\nCompiling Clojurescript...\n\n" | tee -a $LOG_FILE
+logmsg "Compiling Clojurescript..."
 lein cljsbuild once prod &>> $LOG_FILE
 
-echo -en "\n\nCompiling Clojure...\n\n" | tee -a $LOG_FILE
+logmsg "Compiling Clojure..."
 lein uberjar &>> $LOG_FILE
 
 let TIME_TAKEN=(`date +%s`-$START_TIME)
-echo -en "\n\nBuild finished at $(date) in ${TIME_TAKEN} seconds" | tee -a $LOG_FILE
+logmsg "Build finished at $(date) in ${TIME_TAKEN} seconds"
--- modules/jinteki/manifests/mongodb.pp
+++ modules/jinteki/manifests/mongodb.pp
@@ -2,5 +2,10 @@ class jinteki::mongodb {
   ensure_packages([
     'mongodb',
   ])
-
+  service {'mongodb':
+    ensure => running,
+    hasstatus => true,
+    hasrestart => true,
+    require => Package['mongodb'],
+  }
 }

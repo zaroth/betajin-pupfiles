091608189aedd185fd2548f57a2dfb127d8d496f
Lukasz Dobrogowski <lukasz.dobrogowski@gmail.com>
2016-10-03 19:54:38 +0000

    added cron job with rebuild every 6 hours

--- modules/jinteki/manifests/scripts.pp
+++ modules/jinteki/manifests/scripts.pp
@@ -9,4 +9,12 @@ class jinteki::scripts {
     group => 'root',
     mode => '0755',
   }
+
+  cron {'rebuild':
+    command => '/usr/local/bin/rebuild.sh',
+    user => 'jinteki',
+    hour => [0, 6, 12, 18],
+    minute => 0,
+    require => [File['/usr/local/bin/rebuild.sh']],
+  }
 }

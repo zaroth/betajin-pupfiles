6e12249969c741941811c88424660b31b52d1a79
Lukasz Dobrogowski <lukasz.dobrogowski@gmail.com>
2016-10-03 19:47:13 +0000

    added options to rebuild script, fixes #2 #3

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Just run `./setup.sh dryrun` to confirm nothing is out of whack, then `./setup.sh update`. Prepared for Arch Linux only ATM.
 
-Currently not automated, you need to complete the tasks below manually BEFORE running the puppet:
+The following are currently not automated and you need to complete these manually BEFORE running the puppet for the first time:
 
-* Installation of `cower, pacaur`
+* Installation of `cower`, `pacaur`
 * Installation of `jdk` or `jdk-arm` from AUR
--- modules/jinteki/files/scripts/rebuild.sh
+++ modules/jinteki/files/scripts/rebuild.sh
@@ -1,16 +1,53 @@
 #!/bin/bash
 
-if [ ! "$(whoami)" == "jinteki" ]; then
+if [ "$(whoami)" != "jinteki" ]; then
   echo "This script needs to be run as 'jinteki' user. (use the sudo, Luke!)" >&2
   exit 1
 fi
 
-ROOT_DIR='/opt/jinteki'
-REPO_DIR="${ROOT_DIR}/netrunner"
-LOG_DIR="${ROOT_DIR}/logs"
+root_dir='/opt/jinteki'
+repo_dir="${root_dir}/netrunner"
+log_dir="${root_dir}/logs"
+pidfile_path="${root_dir}/rebuild.pid"
 
-LOG_FILE="${LOG_DIR}/$(date +%Y%m%d_%H%M)_build_log.txt"
-START_TIME=`date +%s`
+log_file="${log_dir}/$(date +%Y%m%d_%H%M)_build_log.txt"
+start_time=`date +%s`
+
+function print_help {
+  echo -e "$0 - pull latest code changes for jinteki.net locally and recompile it if needed"
+  echo -e "     Must be run as the user 'jinteki'."
+  echo -e "  Syntax: $0 [OPTIONS]"
+  echo -e "  Options:"
+  echo -e "    -h, --help - print this message and exit"
+  echo -e "    -u, --update - update the dependencies instead of just making sure they're installed"
+  echo -e "    -f, --full - force full recompilation even if the binaries and/or code seems up-to-date"
+}
+
+full_rebuild=false
+kill_process=false
+update_deps=false
+
+while [[ $# -gt 1 ]]; do
+  case $1 in
+    '-f'|'--full')
+      full_rebuild=true
+      ;;
+    '-k'|'--kill')
+      kill_process=true
+      ;;
+    '-h'|'--help')
+      print_help
+      exit 0
+      ;;
+    '-u'|'--update')
+      update_deps=true
+      ;;
+    *)
+      break
+      ;;
+  esac
+  shift
+done
 
 # for commands that don't respect the "no colors in piped output" rule
 function stripcolors {
@@ -19,49 +56,82 @@ function stripcolors {
 
 function logmsg {
   echo "${1}"
-  echo -en "\n\n${1}\n\n" >> $LOG_FILE
+  echo -en "\n\n${1}\n\n" >> $log_file
 }
 
-cd $REPO_DIR
+if [ -f $pidfile_path ]; then
+  previous_pid=$(cat $pidfile_path)
+  if [ "${kill_process}" = true ]; then
+    logmsg "Process already running, with pid ${previous_pid}, killing..."
+    kill $previous_pid
+    rm $pidfile_path
+  else
+    logmsg "Process already running with pid ${previous_pid}, exiting..."
+    exit 1
+  fi
+fi
+
+# from that point on we have to delete our own pidfile on exit
+function cleanup_pidfile {
+  rm $pidfile_path
+}
+
+echo $$ > $pidfile_path
+trap cleanup_pidfile EXIT
+
+cd $repo_dir
 
 logmsg "Pulling fresh changes from GitHub..."
-git fetch &>> $LOG_FILE
-NEW_COMMITS=`git rev-list HEAD...origin/dev --count`
+git fetch &>> $log_file
+new_commits=`git rev-list HEAD...origin/dev --count`
 
-if (( $NEW_COMMITS == 0 )); then
+if (( $new_commits == 0 )) && [ "${full_rebuild}" != true ]; then
   logmsg "No new commits in GitHub dev branch, exiting."
   exit 0
 fi
 
-git pull origin dev &>> $LOG_FILE
+git pull origin dev &>> $log_file
+logmsg "Current commit:"
+git log -1 &>> $log_file
 
-logmsg "Updating npm packages..."
-npm update &>> $LOG_FILE
-npm prune &>> $LOG_FILE
-
-logmsg "Updating bower packages..."
-bower update &>> $LOG_FILE
-bower prune &>> $LOG_FILE
+if [ "${update_deps}" = true ]; then
+  logmsg "Updating npm and bower packages..."
+  npm update &>> $log_file
+  bower update &>> $log_file
+else
+  logmsg "Installing npm and bower packages..."
+  npm install &>> $log_file
+  bower install &>> $log_file
+fi
+# prune only for full rebuild
+if [ "${full_rebuild}" = true ]; then
+  logmsg "Pruning npm and bower packages..."
+  npm prune &>> $log_file
+  bower prune &>> $log_file
+fi
 
 logmsg "Pulling new cards from NRDB..."
-coffee "data/fetch.coffee" &>> $LOG_FILE
+coffee "data/fetch.coffee" &>> $log_file
 
-logmsg "Cleaning up previous build..."
-lein clean &>> $LOG_FILE
+if [ "${full_rebuild}" ]; then
+  logmsg "Cleaning up previous build..."
+  lein clean &>> $log_file
+fi
 
 logmsg "Compiling Stylus files..."
-stylus src/css/ -o resources/public/css/ 2>&1 | stripcolors >> $LOG_FILE
+stylus src/css/ -o resources/public/css/ 2>&1 | stripcolors >> $log_file
 
 logmsg "Compiling ClojureScript..."
-lein cljsbuild once prod 2>&1 | stripcolors >> $LOG_FILE
+lein cljsbuild once prod 2>&1 | stripcolors >> $log_file
 
 logmsg "Compiling Clojure..."
-lein uberjar &>> $LOG_FILE
+lein uberjar &>> $log_file
 
 logmsg "Restarting servers..."
 sudo systemctl restart jinteki-site.service
 sudo systemctl restart jinteki-game.service
 
-let TIME_TAKEN=(`date +%s`-$START_TIME)
-logmsg "Build finished at $(date) in ${TIME_TAKEN} seconds"
+let time_taken=(`date +%s`-$start_time)
+logmsg "Build finished at $(date) in ${time_taken} seconds"
+rm $pidfile_path
 exit 0
